f63532bdd0ac0f7c8a7f0d3778e20a9f5df48408
Gustavo Magnago <ext.gustavo.magnago@farfetch.com>
2024-05-09 19:59:12 +0300

    Adding dist

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter as Router } from 'react-router-dom';
+
 import { ThemeProvider } from '@mui/material/styles';
-import './index.css'
+import App from './App.tsx';
+import './index.css';
 import theme from './theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-    <App />
+      <Router>
+        <App />
+      </Router>
     </ThemeProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
---
+++ src/pages/privacy-policy/index.tsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+import ReactMarkdown from 'react-markdown';
+
+import { Container } from '@mui/material';
+
+function PrivacyPolicy() {
+  const [policyText, setPolicyText] = useState('');
+
+  useEffect(() => {
+    fetch('/privacyPolicy.md')
+      .then((response) => response.text())
+      .then((text) => {
+        setPolicyText(text.replace('[!NOTE]', ''));
+      })
+      .catch((error) => {
+        console.error('Error fetching privacy policy:', error);
+        setPolicyText(error);
+      });
+  }, []);
+
+  return (
+    <Container fixed>
+      <ReactMarkdown>{policyText}</ReactMarkdown>
+    </Container>
+  );
+}
+
+export default PrivacyPolicy;

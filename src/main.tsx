e2818c9c28f3c7104b302d8fce3d65bd4d4deba7
Gustavo Magnago <ext.gustavo.magnago@farfetch.com>
2024-05-13 13:06:49 +0300

    to commit

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,8 @@ import theme from './theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
   </React.StrictMode>
 );
